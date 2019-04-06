3ede01d5c7ab0d52de9c2672f73047f63eafbe47
TC Asoluka <austinjerrytech@users.noreply.github.com>
2019-04-06 19:44:11 +0100

    Index page updated to show comments and platform

--- hng/index.php
+++ hng/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-	<?php include'includes/head.php'; ?>
+	<?php include 'includes/head.php'; ?>
 	<link rel="stylesheet" href="style.css" type="text/css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <body>
@@ -8,50 +8,28 @@
 		<div class="container padd">
 			<div class="row">
 				<div class="col-sm-3">
-					<div class="row">
-						<div class="col-sm-9">
-							<div class="row">
-								<img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
-								<div class="row text-center">
-									<h3>TONI ATUNRASE</h3>
-									<h4 class="occupation">Web/Software Developer</h4>
-									<p>Software Enginner at AWS design enthusiast and music lover</p>
-									<i class="fa fa-medium"></i>
-									<i class="fa fa-github"></i>
-									<i class="fa fa-twitter"></i>
-								</div>
-							</div>
-							<hr>
-							<div class="row">
-								<ul class="side_nav">
-								  <li><a class="side_nav" href="#home">Home</a></li>
-								  <li><a class="side_nav" href="#news">News</a></li>
-								  <li><a class="side_nav" href="#contact">Contact</a></li>
-								  <li><a class="side_nav" href="#about">About</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
+					<?php include'includes/nav.php'; ?>
 				</div>
 
 				<div class="col-sm-9">
 					<div class="row">
 						<div class="col-sm-12">
 							<div class="col-sm-12 status-area">
-								<input class="form-control" type="text" name="blog_title" placeholder="Title">
-								<div class="toolbar">
-									<button class="tool fa fa-underline"  onclick="document.execCommand('underline', false, '');"></button>
-									<button class="tool fa fa-italic" onclick="document.execCommand('italic', false, '');"></button>
-									<button class="tool fa fa-bold" onclick="document.execCommand('bold', false, '');"></button>
-									<button class="tool fa fa-scissors" onclick="document.execCommand('cut',false,'')"></button>
-									<button class="tool fa fa-strikethrough" onclick="document.execCommand('strikeThrough',false,'')"></button>
-									<button class="tool fa fa-trash" onclick="document.execCommand('delete',false,'')"></button>
-									<button class="tool fa fa-scribd" onclick="document.execCommand('selectAll',false,'')"></button>
-									<button class="tool fa fa-align-center" onclick="document.execCommand('justifyCenter',false,'')"></button>
-									<button class="tool fa fa-align-left" onclick="document.execCommand('justifyLeft',false,'')"></button>
-									<button class="tool fa fa-align-right" onclick="document.execCommand('justifyRight',false,'')"></button>								</div>
+								<input class="form-control col-sm-12" type="text" name="blog_title" placeholder="Title">
+									<div class="toolbar">
+										<button class="tool fa fa-underline"  onclick="document.execCommand('underline', false, '');"></button>
+										<button class="tool fa fa-italic" onclick="document.execCommand('italic', false, '');"></button>
+										<button class="tool fa fa-bold" onclick="document.execCommand('bold', false, '');"></button>
+										<button class="tool fa fa-scissors" onclick="document.execCommand('cut',false,'')"></button>
+										<button class="tool fa fa-strikethrough" onclick="document.execCommand('strikeThrough',false,'')"></button>
+										<button class="tool fa fa-trash" onclick="document.execCommand('delete',false,'')"></button>
+										<button class="tool fa fa-scribd" onclick="document.execCommand('selectAll',false,'')"></button>
+										<button class="tool fa fa-align-center" onclick="document.execCommand('justifyCenter',false,'')"></button>
+										<button class="tool fa fa-align-left" onclick="document.execCommand('justifyLeft',false,'')"></button>
+										<button class="tool fa fa-align-right" onclick="document.execCommand('justifyRight',false,'')"></button>
+									</div>
 								<div class="editor" contenteditable>
-									<p class="editor__text">Please enter a post </p>
+								<input class="form-control col-sm-12" type="text" name="blog_text" placeholder="Tell your story...">
 								</div>
 							</div>
 						</div>
@@ -71,6 +49,13 @@
 									<div class="col-sm-10">
 										<h6>April 10th, 2019</h6>
 										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+
+										<div class="col-sm-12">
+										<div class="row">
+											<div class="col-xs-6 text-left"><div class="row"><h6>The verge</h6></div></div>
+											<div class="col-xs-6 text-right"><h6>Comments</h6></div>
+										</div>
+									</div>
 									</div>
 								</div>
 
@@ -81,6 +66,13 @@
 									<div class="col-sm-10">
 										<h6>April 10th, 2019</h6>
 										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+
+										<div class="col-sm-12">
+										<div class="row">
+											<div class="col-xs-6 text-left"><div class="row"><h6>The verge</h6></div></div>
+											<div class="col-xs-6 text-right"><h6>Comments</h6></div>
+										</div>
+									</div>
 									</div>
 								</div>
 
@@ -91,6 +83,13 @@
 									<div class="col-sm-10">
 										<h6>April 10th, 2019</h6>
 										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+
+										<div class="col-sm-12">
+										<div class="row">
+											<div class="col-xs-6 text-left"><div class="row"><h6>The verge</h6></div></div>
+											<div class="col-xs-6 text-right"><h6>Comments</h6></div>
+										</div>
+									</div>
 									</div>
 								</div>
 
@@ -101,6 +100,30 @@
 									<div class="col-sm-10">
 										<h6>April 10th, 2019</h6>
 										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+
+										<div class="col-sm-12">
+										<div class="row">
+											<div class="col-xs-6 text-left"><div class="row"><h6>The verge</h6></div></div>
+											<div class="col-xs-6 text-right"><h6>Comments</h6></div>
+										</div>
+									</div>
+									</div>
+								</div>
+
+								<div class="row post">
+									<div class="col-sm-2">
+										<img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
+									</div>
+									<div class="col-sm-10">
+										<h6>April 10th, 2019</h6>
+										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+
+										<div class="col-sm-12">
+										<div class="row">
+											<div class="col-xs-6 text-left"><div class="row"><h6>The verge</h6></div></div>
+											<div class="col-xs-6 text-right"><h6>Comments</h6></div>
+										</div>
+									</div>
 									</div>
 								</div>
 							<div class="col-sm-6"></div>
